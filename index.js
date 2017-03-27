c5490da6c9b34df677179afdb6fd87f6929d27c6
louisGan <louisGan1988@gmail.com>
2017-03-27 11:21:01 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+
+/**
+ *   时间转化方法
+ *
+ *
+ **/
+export function timeago(date) {
+  const MathFloor = Math.floor;
+  date = new Date(date);
+  const dateYear = date.getFullYear();
+  const dateMonths = date.getMonth() + dateYear * 12; // 计算总月数
+  const time = date.getTime();
+  const curDate = new Date();
+  const curDateYear = curDate.getFullYear();
+  const curDateMonths = curDate.getMonth() + curDateYear * 12; // 计算总月数
+  const curTime = curDate.getTime();
+  const deltaTime = MathFloor((curTime - time) / 1000); // 转为秒
+
+  if (curDateMonths - dateMonths > 12) {
+    return `${Math.floor((curDateMonths - dateMonths) / 12)}年前`;
+  } else if (curDateMonths - dateMonths > 1) {
+    return `${curDateMonths - dateMonths}个月前`;
+  } else {
+    if (deltaTime < 60) {
+      return `${deltaTime}秒前`;
+    } else if (deltaTime < 3600) {
+      return `${MathFloor(deltaTime / 60)}分钟前`;
+    } else if (deltaTime < 86400) {
+      return `${MathFloor(deltaTime / 3600)}小时前`;
+    } else if (deltaTime < 604800) {
+      return `${MathFloor(deltaTime / 86400)}天前`;
+    } else {
+      return `${MathFloor(deltaTime / 604800)}周前`;
+    }
+  }
+}
+
+
